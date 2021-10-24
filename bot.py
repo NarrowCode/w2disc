3b3d167449d144ed5a7dc25dc2781c84ee154395
Andreas Steinkellner <asteinkellner1@gmail.com>
2021-10-24 20:38:17 +0200

    Change from using a selenium web-scraping approach to the official w2g api

--- bot.py
+++ bot.py
@@ -1,96 +1,109 @@
 import os
+from typing import Any
 from dotenv import load_dotenv
-from selenium.webdriver import Firefox
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+import requests
+import json
 from time import sleep
 import asyncio
 
 from discord.ext import commands
 
-URL = 'https://w2g.tv/'
+# API documentation: https://community.w2g.tv/t/watch2gether-api-documentation/133767
+W2G_URL = 'https://w2g.tv/rooms/create.json'
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+W2G_API_KEY = os.getenv("W2G_API_KEY")
 
-opts = Options()
-opts.headless = True
-opts.add_argument("--mute-audio")
-#opts.binary = firefox_binary
-assert opts.headless
-global browser
-browser = Firefox(options = opts)
-firefox_profile = FirefoxProfile()
-firefox_profile.set_preference("media.volume_scale", "0.0")
+CURRENT_ROOM_KEY = ""
+ROOMS_CREATED = 0
 
-current_room_url = ""
+W2G_HEADERS = {"Accept": "application/json", "Content-Type": "application/json"}
+
+def make_url_string():
+  return f"https://w2g.tv/rooms/{CURRENT_ROOM_KEY}"
 
 def create_room(yt_url):
-  global browser
-  browser = Firefox(options = opts, firefox_profile = firefox_profile)
-  browser.maximize_window()
-  browser.get(URL)
-  try:
-    WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div/div[2]/div/button[2]'))).click()
-  except:
-    print("Could not click cookie accept thingy.")
+  post_req = {"w2g_api_key": W2G_API_KEY, "share": yt_url, "bg_color": "#2d3c4a", "bg_opacity": "50"}
+  json_post = json.dumps(post_req, indent = 4)
+  res = requests.post(W2G_URL, data = json_post, headers = W2G_HEADERS)
+  json_res = json.loads(res.content)
+  stream_key = json_res["streamkey"]
+  global CURRENT_ROOM_KEY
+  CURRENT_ROOM_KEY = stream_key
+  global ROOMS_CREATED
+  ROOMS_CREATED =+ 1
+  return make_url_string()
 
-  try:
-    WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.ID, 'create_room_button'))).click()
-    WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/div[2]/div'))).click()
-    url_field = browser.find_element(By.ID, 'search-bar-input')
-    url_field.send_keys(yt_url)
-    WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div[1]/div[4]/div[2]/form/div/button'))).click()
-    WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div[2]/div[1]/div[1]/div/div[5]/div[4]/div/div[1]'))).click()
-  except:
-    print("Something broke :<")
-  #/html/body/div[1]/div/div/div/div[2]/div/button[2]                     # Cookie  
-  #/html/body/div[1]/div[2]/div[2]/div                                    # Join room button
-  #/html/body/div[2]/div[1]/div[4]/div[2]/form/div/button                 # Search
-  #/html/body/div[2]/div[2]/div[1]/div[1]/div/div[5]/div[4]/div/div[1]    # Element
+def add_to_playlist(yt_url):
+  post_req = {"w2g_api_key": W2G_API_KEY, "add_items": [{"url": yt_url}]}
+  json_post = json.dumps(post_req, indent = 4)
+  res = requests.post(f"https://w2g.tv/rooms/{CURRENT_ROOM_KEY}/playlists/current/playlist_items/sync_update", data = json_post, headers = W2G_HEADERS)
+  if (res.status_code == 200):
+    return "Added successfully!"
+  else:
+    return "Could not add to playlist ..."
 
-  final_url = browser.current_url
-  return final_url
+def get_room():
+  return CURRENT_ROOM_KEY
 
 bot = commands.Bot(command_prefix="$")
 
 @bot.command()
 async def w2g(ctx, arg):
-  working_msg = await ctx.channel.send("working ...")
 
   room_url = create_room(arg)
-  global current_room_url
-  current_room_url = room_url
   room_msg = await ctx.channel.send(room_url)
 
-  await asyncio.sleep(20)
   await ctx.message.delete()
-  await working_msg.delete()
+  await asyncio.sleep(120)
   await room_msg.delete()
-  global browser
-  os.system("pkill -f firefox-esr")
-  browser.close()
 
 @bot.command()
 async def room(ctx):
-  if current_room_url != "":
-    await ctx.channel.send(current_room_url)
+  room_msg: Any
+  if CURRENT_ROOM_KEY != "":
+    room_msg = await ctx.channel.send(make_url_string())
   else:
-    await ctx.channel.send("no current room found. create one with: $w2g url")
+    room_msg = await ctx.channel.send("no current room found. create one with: $w2g url")
+  
+  await ctx.message.delete()
+  ctx.message.user
+  await asyncio.sleep(120)
+  await room_msg.delete()
 
 @bot.command()
-async def close(ctx):
-  global browser
-  browser.close()
-  await ctx.channel.send("browser closed.")
+async def add(ctx, arg):
+  room_msg = Any
+  if CURRENT_ROOM_KEY == "":
+    room_msg = await ctx.channel.send("no current room found. create one with: $w2g url")
+  else:
+    result = add_to_playlist(arg)
+    room_msg = await ctx.channel.send(result)
+  
+  await ctx.message.delete()
+  await asyncio.sleep(20)
+  await room_msg.delete()
+
 
 @bot.command()
 @commands.is_owner()
 async def terminate(ctx):
-  await ctx.channel.send("me ded.")
+  room_msg = await ctx.channel.send("terminating bot.")
+
+  await asyncio.sleep(20)
+  await ctx.message.delete()
+  await room_msg.delete()
+  quit()
+
+@bot.command()
+@commands.is_owner()
+async def stats(ctx):
+  room_msg = await ctx.channel.send(f"Total rooms created: {ROOMS_CREATED}")
+
+  await asyncio.sleep(20)
+  await ctx.message.delete()
+  await room_msg.delete()
   quit()
 
 bot.run(TOKEN)
